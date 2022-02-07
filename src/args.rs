96aaea8b3419eb46352bc7175ab169837c20eac9
Arnaud Gourlay <arnaud.gourlay@gmail.com>
2022-02-07 20:51:17 +0100

    next version

--- src/args.rs
+++ src/args.rs
@@ -7,7 +7,7 @@ use std::path::Path;
 
 fn app() -> clap::App<'static> {
     App::new("dlm")
-        .version("0.2.4")
+        .version("0.2.5")
         .author("Arnaud Gourlay <arnaud.gourlay@gmail.com>")
         .about("Minimal download manager")
         .arg(
